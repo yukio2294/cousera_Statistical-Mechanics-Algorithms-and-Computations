50f344d37b25a88f7abaea11f810d3c05a904390
yukio2294 <yuki.o2294@gmail.com>
2018-02-13 10:24:00 +0900

    add *.py

---
+++ homework_1/c2.py
@@ -0,0 +1,17 @@
+import random, math
+n_trials = 400000
+n_hits = 0
+var = 0.0
+Obs1 = 0
+Obs2 = 0
+for iter in range(n_trials):
+    x, y = random.uniform(-1.0, 1.0), random.uniform(-1.0, 1.0)
+    Obs = 0.0
+    if x**2 + y**2 < 1.0:
+        n_hits += 1
+        Obs = 4.0
+    Obs1 += (Obs)**2
+    Obs2 += Obs
+var = (Obs1 / n_trials) - (Obs2 / n_trials)**2
+print 4.0 * n_hits / float(n_trials), math.sqrt(var)
+print math.sqrt(var) / math.sqrt(float(n_trials))
